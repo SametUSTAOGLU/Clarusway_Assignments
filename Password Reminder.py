fdf8995231847dcf5d944cdb760ca98ed726375b
SametUSTAOGLU <ustaoglusamet05@gmail.com>
2021-07-13 10:14:42 +0300

    password reminder

---
+++ Password Reminder.py
@@ -0,0 +1,6 @@
+my_name = "samet".title()
+input_name = input("Please enter your name: ").title()
+if my_name == input_name:
+    print(f"Hello, {input_name}! The password is : W@12")
+else:
+    print(f"Hello, {input_name}! See you later.")
